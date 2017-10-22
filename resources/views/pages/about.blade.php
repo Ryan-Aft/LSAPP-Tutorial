81b3e3cdd188dbf45511cef142ddecb029714944
Ryan Aft <ryanaft@gmail.com>
2017-10-21 21:49:34 -0400

    Firts Commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+
+
+@extends ('layouts.app')
+
+@section('content')
+    <h1>This is My Apps about page</h1>
+
+    <input color = Type= "text">
+    <p>This is a paragraph relevant to this about page</p>
+@endsection
+
